fbae0f0e35a45327f1622ec58397a15fa3bc9e49
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-04-07 00:54:14 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,10 +5,10 @@ from helpers import random_nonce, compress_point
 from curve import Point, Secp256k1
 from keys import Secp256k1_new
 
-def message_sign():
+def test_sign_and_verify():
     # optional: re-use existing keypair
     secp256k1 = Secp256k1()
     keypair = Secp256k1_new(secp256k1.curve)
     signature_json = secp256k1.new(keypair, "Jonas und Kristiana luv much")
     secp256k1.verify(signature_json)
-message_sign()
+test_sign_and_verify()
